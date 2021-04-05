c118d28438d170993855bf15c7268913d595ae78
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-04-05 18:16:23 +0300

    finish lab12

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require('express');
 // const { Console } = require('node:console');
 const superagent = require('superagent');
 const app = express();
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL)
 app.set('view engine', 'ejs');
 const PORT = process.env.PORT;
 app.use(express.static('./public/'));
@@ -16,7 +18,22 @@ app.get('/test', (req, res)=> {
     res.render('pages/index')
 })
 app.get('/', (req, res)=> {
-    res.render('pages/index')
+    let SQL = 'SELECT * FROM books';
+    client.query(SQL).then(response => {
+        let result = response.rows;
+        let count = result.length;
+        res.render('pages/index', {myBooks: result, num: count})
+    })
+})
+app.get('/books/details/:id', (req,res)=>{
+    let SQL = 'SELECT * FROM books WHERE id=$1';
+    let ids = req.params.id;
+    client.query(SQL,[ids]).then(response => {
+        let result = response.rows[0];
+        console.log(result)
+        res.render('pages/searches/detail', {myBooks: result})
+    })
+    
 })
 let bookArr = [];
 function Book(t,a,d,src,isbn){
@@ -37,14 +54,14 @@ app.post('/searches', (req, res) => {
     superagent.get(url).then( result => {
         let books = result.body.items;
         
-        
+        if(result.body.totalItems){
         books.forEach(element => {
             
             let reg = /https/gi;
             let title = element.volumeInfo.title;
             let author = ''
             if(element.volumeInfo.authors){
-            author = element.volumeInfo.authors.join(' and ');
+            author = element.volumeInfo.authors.join(' + ');
             }else{
                 author = 'no author available'
             }
@@ -67,11 +84,28 @@ app.post('/searches', (req, res) => {
                 
 
             }
-        });
+        });}else {
+            new Book()
+        }
     
         
         res.render('pages/searches/show', {myBooks: bookArr})
     })
+})
+app.post('/', (req,res)=>{
+    let info = req.body.two;
+    
+    let values = [info[2],info[1],info[4],info[0],info[3]];
+    let SQL = 'INSERT INTO books (author, title, isbn, image_url, description) VALUES ( $1, $2, $3, $4, $5) RETURNING *';
+    
+    client.query(SQL, values).then( (y)=> {
+        console.log(y.rows)
+        let n = y.rows[0].id;
+        res.redirect(`/books/details/${n}`)
+    }).catch(err => {
+        console.log(err)
+    })
+    
 })
 app.use('*', notFoundHandler); // 404 not found url
  
@@ -85,4 +119,7 @@ function errorHandler(err, request, response, next) {
   response.status(500).render('pages/error');
 }
 // book image author title isbn description
-app.listen(PORT , ()=> console.log(`I'm using ${PORT} port`))
+client.connect().then(()=>{
+
+    app.listen(PORT , ()=> console.log(`I'm using ${PORT} port`))
+})
